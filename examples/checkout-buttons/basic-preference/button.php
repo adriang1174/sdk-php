8bc048232f3fe763e6f9ccf184c5583e62241af6
Anonymous <adriang_1174@hotmail.com>
2015-09-07 16:50:56 +0000

    first draft

--- examples/checkout-buttons/basic-preference/button.php
+++ examples/checkout-buttons/basic-preference/button.php
@@ -1,13 +1,13 @@
 <?php
 require_once "../../../lib/mercadopago.php";
 
-$mp = new MP("YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET");
+$mp = new MP("1362883408014966", "bZCd0dFXDFFN26CR2kAml3I7kLoJx6cK");
 
 $preference_data = array(
     "items" => array(
         array(
             "title" => "Title of what you are paying for",
-            "currency_id" => "USD",
+            "currency_id" => "ARS",
             "category_id" => "Category",
             "quantity" => 1,
             "unit_price" => 10.2
@@ -24,7 +24,7 @@ $preference = $mp->create_preference($preference_data);
         <title>MercadoPago SDK - Create Preference and Show Checkout Example</title>
     </head>
     <body>
-       	<a href="<?php echo $preference["response"]["init_point"]; ?>" name="MP-Checkout" class="orange-ar-m-sq-arall">Pay</a>
+       	<a href="<?php echo $preference["response"]["sandbox_init_point"]; ?>" name="MP-Checkout" class="orange-ar-m-sq-arall">Pay</a>
         <script type="text/javascript" src="http://mp-tools.mlstatic.com/buttons/render.js"></script>
     </body>
 </html>
