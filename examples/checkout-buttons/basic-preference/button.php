2236a23cd7b173f02237bd3a7f39fc48a70a29be
Anonymous <adriang_1174@hotmail.com>
2015-09-07 17:22:36 +0000

    monto-variable

--- examples/checkout-buttons/basic-preference/button.php
+++ examples/checkout-buttons/basic-preference/button.php
@@ -1,21 +1,26 @@
 <?php
 require_once "../../../lib/mercadopago.php";
 
-$mp = new MP("1362883408014966", "bZCd0dFXDFFN26CR2kAml3I7kLoJx6cK");
 
-$preference_data = array(
-    "items" => array(
-        array(
-            "title" => "Title of what you are paying for",
-            "currency_id" => "ARS",
-            "category_id" => "Category",
-            "quantity" => 1,
-            "unit_price" => 10.2
-        )
-    )
-);
-
-$preference = $mp->create_preference($preference_data);
+if (!empty($_REQUEST['precio']))
+{
+	$mp = new MP("1362883408014966", "bZCd0dFXDFFN26CR2kAml3I7kLoJx6cK");
+	
+	$preference_data = array(
+	    "items" => array(
+	        array(
+	            "title" => "Aca va el texto de la descripcion de lo que se está pagando",
+	            "currency_id" => "ARS",
+	            "category_id" => "Category",
+	            "quantity" => 1,
+	            "unit_price" => $_REQUEST['precio']
+	        )
+	    )
+	);
+	
+	$preference = $mp->create_preference($preference_data);
+	header("Location: ". $preference["response"]["sandbox_init_point"]);
+}
 ?>
 
 <!doctype html>
@@ -24,7 +29,11 @@ $preference = $mp->create_preference($preference_data);
         <title>MercadoPago SDK - Create Preference and Show Checkout Example</title>
     </head>
     <body>
-       	<a href="<?php echo $preference["response"]["sandbox_init_point"]; ?>" name="MP-Checkout" class="orange-ar-m-sq-arall">Pay</a>
+    	<form action="" method="post">
+    		<input type="text" name="precio" id="precio />
+    		<button type="submit" class="orange-ar-m-sq-arall">Pagar</button>
+    	</form>
+<!--       	<a href="<?php echo $preference["response"]["sandbox_init_point"]; ?>" name="MP-Checkout" class="orange-ar-m-sq-arall">Pay</a> -->
         <script type="text/javascript" src="http://mp-tools.mlstatic.com/buttons/render.js"></script>
     </body>
 </html>
